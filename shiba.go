94d6bc3df1a4171274db3e07ed6de020df00447d
kubohisa <kubohisa@gmail.com>
2024-04-26 20:09:12 +0900

    Update shiba.go
    Go fmt.

--- shiba.go
+++ shiba.go
@@ -3,36 +3,36 @@ package main
 import (
 	"fmt"
 	"time"
-	
+
 	"net"
 	"net/http"
 
 	"golang.org/x/time/rate"
 	"log"
-  
-  	"context"
+
+	"context"
 	"os"
 	"os/signal"
-	
-    "runtime"
-    "runtime/debug"
+
+	"runtime"
+	"runtime/debug"
 )
 
 // System Settings.
 const (
 	filePath string = "public"
-	
+
 	host string = "127.0.0.1"
 	port string = "8000"
-	
+
 	urlLength int = 1000
-	
+
 	frameCount int = 10
 )
 
-var(
-	urlSetting []*urlRotator = []*urlRotator {
-		{"/" , world},
+var (
+	urlSetting []*urlRotator = []*urlRotator{
+		{"/", world},
 	}
 )
 
@@ -42,7 +42,7 @@ var (
 
 //
 type urlRotator struct {
-	path string
+	path     string
 	function func(w http.ResponseWriter, r *http.Request)
 }
 
@@ -51,23 +51,23 @@ func main() {
 	//
 	mux := http.NewServeMux()
 	for _, f := range urlSetting {
-		mux.HandleFunc("/exec" + f.path, f.function)
+		mux.HandleFunc("/exec"+f.path, f.function)
 	}
 	mux.HandleFunc("/", publicFile)
-	
+
 	urlSetting = nil // メモリから削除
 
 	//
 	server := &http.Server{
 		Addr:    net.JoinHostPort(host, port),
 		Handler: start(mux),
-		
+
 		ReadHeaderTimeout: 10 * time.Second,
-		ReadTimeout: 10 * time.Second,
+		ReadTimeout:       10 * time.Second,
 	}
-	
-    runtime.GC()
-    debug.SetGCPercent(-1)
+
+	runtime.GC()
+	debug.SetGCPercent(-1)
 
 	fmt.Println("start Shiba server.")
 
@@ -77,42 +77,42 @@ func main() {
 	go func() {
 		log.Fatal(server.ListenAndServe())
 	}()
-	
+
 	<-ctx.Done()
-	ctx, cancel := context.WithTimeout(context.Background(), 14 * time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 14*time.Second)
 	defer cancel()
-	
-	fmt.Println("shutdown.")	
+
+	fmt.Println("shutdown.")
 	server.Shutdown(ctx)
-	
-    debug.SetGCPercent(100)
+
+	debug.SetGCPercent(100)
 }
 
-var	frameCounter int = 0
+var frameCounter int = 0
 
 func start(next http.Handler) http.Handler {
 	//
 	frameCounter++
-	if (frameCounter == frameCount) {
+	if frameCounter == frameCount {
 		frameCounter = 0
-		
+
 		rtsGarbageCollection()
 	}
-	
+
 	//
-    return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-        if limiter.Allow() == false {
-            http.Error(w, http.StatusText(429), http.StatusTooManyRequests)
-            return
-        }
-		
-		if (len(r.URL.Path) > urlLength) {
-            http.Error(w, http.StatusText(500), http.StatusInternalServerError)
-            return			
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if limiter.Allow() == false {
+			http.Error(w, http.StatusText(429), http.StatusTooManyRequests)
+			return
 		}
 
-        next.ServeHTTP(w, r)
-    })
+		if len(r.URL.Path) > urlLength {
+			http.Error(w, http.StatusText(500), http.StatusInternalServerError)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
 }
 
 func rtsGarbageCollection() {
@@ -120,13 +120,13 @@ func rtsGarbageCollection() {
 
 	runtime.ReadMemStats(&mem)
 
-	if mem.HeapAlloc > 500 << 20 { // 500MB
+	if mem.HeapAlloc > 500<<20 { // 500MB
 		runtime.GC()
-	}	
+	}
 }
 
 func publicFile(w http.ResponseWriter, r *http.Request) {
-	http.ServeFile(w, r, filePath + r.URL.Path)
+	http.ServeFile(w, r, filePath+r.URL.Path)
 }
 
 //
